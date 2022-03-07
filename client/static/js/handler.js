8f8a45c2df00bfc76f43e7ae5b1217996f30dff1
idrisimo <idrissilva@hotmail.com>
2022-03-07 15:35:18 +0000

    changed format of article data and started on the fetch however this may need to change later

--- client/static/js/handler.js
+++ client/static/js/handler.js
@@ -2,10 +2,21 @@
 function submitArticle(event) {
     event.preventDefault()
     console.log('form submitted')
-    const title = event.target['articleTitle'].value
-    const text = event.target['articleText'].value
-    console.log(`title: ${title}, text: ${text}, time: ${new Date()}`)
-    
+    const articleData = {
+        title: event.target['articleTitle'].valuele,
+        description: event.target['articleText'].value,
+        createdAt: new Date()
+    };
+    console.log(articleData)
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(articleData),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+    // TODO this fetch will most likely need to change before production
+    fetch('http://localhost:3000/')
 }
 
 module.exports = {submitArticle}
