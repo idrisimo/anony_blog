aa0604287139179403ceb814fca64581f83ebd8c
idrisimo <idrissilva@hotmail.com>
2022-03-07 21:30:23 +0000

    submission success alert added and working

--- client/bundle.js
+++ client/bundle.js
@@ -3,30 +3,61 @@
 function submitArticle(event) {
     event.preventDefault()
     console.log('form submitted')
-    const articleData = {
-        title: event.target['articleTitle'].value,
-        description: event.target['articleText'].value,
-        createdAt: new Date()
-    };
-    console.log(articleData)
-    const options = {
-        method: 'POST',
-        body: JSON.stringify(articleData),
-        headers: {
-            "Content-Type": "application/json"
+    try {
+        const articleData = {
+            title: event.target['articleTitle'].value,
+            description: event.target['articleText'].value,
+            createdAt: new Date()
+        };
+        console.log(articleData)
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(articleData),
+            headers: {
+                "Content-Type": "application/json"
+            }
         }
+        // TODO this fetch will most likely need to change before production
+        fetch('http://localhost:3000/create', options)
+        closeModalOnSuccess()
+        successAlert('Journal entry submitted', 'success')
+    } catch {
+
     }
-    // TODO this fetch will most likely need to change before production
-    fetch('http://localhost:3000/create', options)
 }
 
-module.exports = {submitArticle}
+function closeModalOnSuccess() {
+    const modalElement = document.getElementById('articleModal')
+    const modal = bootstrap.Modal.getInstance(modalElement)
+    modal.hide()
+}
+
+function successAlert(message, type) {
+    const alertWrapper = document.createElement('div')
+    alertWrapper.setAttribute('class', `alert alert-${type} alert-dismissible`)
+    alertWrapper.setAttribute('role', 'alert')
+    const btn = document.createElement('button')
+    btn.setAttribute('type', 'button')
+    btn.setAttribute('class', 'btn-close')
+    btn.setAttribute('data-bs-dismiss', 'alert')
+    btn.setAttribute('aria-label', 'Close')
+    alertWrapper.textContent =  message
+    alertWrapper.append(btn)
+    const submitAlert = document.getElementById('submitAlert')
+    submitAlert.append(alertWrapper)
+ 
+}
+
+module.exports = { submitArticle }
 
 },{}],2:[function(require,module,exports){
 const { submitArticle } = require("./handler");
 
 // selectors
 const articleForm = document.querySelector('#userForm');
+
+const submitAlert = document.getElementById('submitAlert')
+
 
 // event listeners
 articleForm.addEventListener('submit', submitArticle)
--- client/static/js/index.js
+++ client/static/js/index.js
@@ -3,6 +3,9 @@ const { submitArticle } = require("./handler");
 // selectors
 const articleForm = document.querySelector('#userForm');
 
+const submitAlert = document.getElementById('submitAlert')
+
+
 // event listeners
 articleForm.addEventListener('submit', submitArticle)
 
