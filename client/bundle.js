860109dbdd1e4d7220753b383fbbffbdd995ec91
idrisimo <idrissilva@hotmail.com>
2022-03-07 23:35:23 +0000

    added missing updates including favicon (mostly because the 404 was bugging me)

--- client/bundle.js
+++ client/bundle.js
@@ -1,5 +1,71 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 
+
+function buildDeck() {
+
+    fetch('http://localhost:3000/articles')
+        .then((response) => response.json())
+        .then((data) => {
+            const wrapper = document.getElementById('cards')
+            
+            for (index in data) {
+                const card = cardTemplate(data[index])
+                console.log(card)
+                // wrapper.append(card)
+                wrapper.insertAdjacentHTML('afterbegin', card)
+            }
+        })
+}
+
+function cardTemplate(data) {
+    const template = `<div id="cardNum${data['id']}"class="col">
+    <div class="card">
+        <div class="card-header">
+            <i class="fa-solid fa-face-smile-beam "></i>
+            <i class="fa-solid fa-face-smile-beam "></i>
+            <i class="fa-solid fa-face-smile-beam "></i>
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">${data['title']}</h5>
+            <p class="card-text">${data['description']}</p>
+        </div>
+        <div class="card-footer">
+            <div>
+                <a href="#" class="comment-icon-format me-3">
+
+                    <i class="fa-solid fa-comment">
+                        <span
+                            class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger comment-pill">
+                            99+
+                            <span class="visually-hidden">unread messages</span>
+                    </i>
+                </a>
+                <a href="">
+                    <i class="fa-solid fa-face-smile"></i>
+                </a>
+            </div>
+            <small class="text-muted text-end">${data['createdAt']}</small>
+        </div>
+    </div>
+</div>`
+return template
+}
+
+function removeCards(event) {
+    // Skywalker in the jedi temple.
+    event.preventDefault()
+    const wrapper = document.getElementById('cards');
+    let child = wrapper.lastElementChild; 
+    while (child) {
+        wrapper.removeChild(child)
+        child = wrapper.lastElementChild;
+    }
+}
+
+module.exports = { buildDeck, removeCards }
+
+},{}],2:[function(require,module,exports){
+
 function submitArticle(event) {
     event.preventDefault()
     console.log('form submitted')
@@ -36,12 +102,15 @@ function successAlert(message, type) {
     const alertWrapper = document.createElement('div')
     alertWrapper.setAttribute('class', `alert alert-${type} alert-dismissible`)
     alertWrapper.setAttribute('role', 'alert')
+
     const btn = document.createElement('button')
     btn.setAttribute('type', 'button')
     btn.setAttribute('class', 'btn-close')
     btn.setAttribute('data-bs-dismiss', 'alert')
     btn.setAttribute('aria-label', 'Close')
-    alertWrapper.textContent =  message
+
+    alertWrapper.textContent = message
+
     alertWrapper.append(btn)
     const submitAlert = document.getElementById('submitAlert')
     submitAlert.append(alertWrapper)
@@ -50,8 +119,9 @@ function successAlert(message, type) {
 
 module.exports = { submitArticle }
 
-},{}],2:[function(require,module,exports){
+},{}],3:[function(require,module,exports){
 const { submitArticle } = require("./handler");
+const {buildDeck, removeCards} = require("./cardCreation")
 
 // selectors
 const articleForm = document.querySelector('#userForm');
@@ -60,7 +130,11 @@ const submitAlert = document.getElementById('submitAlert')
 
 
 // event listeners
-articleForm.addEventListener('submit', submitArticle)
+articleForm.addEventListener('submit', (event) => {
+    submitArticle(event);
+    removeCards(event);
+    buildDeck()
+})
+document.onload = buildDeck()
 
-
-},{"./handler":1}]},{},[2]);
+},{"./cardCreation":1,"./handler":2}]},{},[3]);
