b37cc4a13ee01f2d7bc38a74e110382ceaac2c29
Abdulrahman Awad <abdul.hmha@gmail.com>
2022-03-10 18:53:24 +0000

    Adding appropriate post content

--- server/data.js
+++ server/data.js
@@ -1,9 +1,9 @@
 const articles =  [
 
-    { id:1, title: "article 1", createdAt:"", description:"Very good article 1",comments: ["Good post", "I like this post"],reactions: ["U+1F600", "U+1F600"], giphys: [null]},
-    { id:2, title: "article 2", createdAt:"", description:"Very good article 2",comments: ["I hate this post", "bad post"],reactions:  [null], giphys: [null]},
-    { id:3, title: "article 3", createdAt:"", description:"Very good article 3",comments: ["OK post", "Not a bad post"],reactions:  ["U+1F600"], giphys: [null]},
-    { id:4, title: "article 4", createdAt:"", description:"Very good article 4",comments: ["Great post! ", "I relate to this post"],reactions: ["U+1F600", "U+1F600", "U+1F602"], giphys: [null]}
+    { id:1, title: "Today I spilt my coffee", createdAt:"", description:"Well that was a big oops. Everyone in the shop saw what happened. Can't get over it",comments: ["Hahaha that happened to me too!", "Don't worry about it"],reactions: ["U+1F600", "U+1F600"], giphys: [null]},
+    { id:2, title: "Feeling happy", createdAt:"", description:"Today's weather is really affecting my mood in a positive way. Hope it stays like this",comments: ["I love days like this.", "Bring on the sun!"],reactions:  [null], giphys: [null]},
+    { id:3, title: "Need some ideas", createdAt:"", description:"Anyone know any new recipes I could try using salmon?",comments: ["You could try putting it on the oven with some herbs, lemon and garlic", "Salmon! YUCK!"],reactions:  ["U+1F600"], giphys: [null]},
+    { id:4, title: "Yeah baby!", createdAt:"", description:"Just got a pay rise once again. I can really see my work paying off.",comments: ["Lucky you :( ", "Make sure you're still saving! Otherwise it will be pointless"],reactions: ["U+1F600", "U+1F600", "U+1F602"], giphys: [null]}
 ]
 
 
