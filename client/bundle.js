762478df8bf520a9d0056c4a1807495dc4bfeccb
idrisimo <idrissilva@hotmail.com>
2022-03-10 00:21:43 +0000

    found the bug. Its always the little ones, hopefully it doesn't have friends

--- client/bundle.js
+++ client/bundle.js
@@ -222,16 +222,37 @@ const { submitReaction } = require("./reactionController");
 window.onload = () => {
 
     buildDeck()
-    
+
     console.log('done')
     const cardDeck = document.getElementById('cards')
 
-    var observer = new MutationObserver(function (mutationRecords) {
+     var observer = new MutationObserver(function (mutationRecords) {
         console.log("change detected");
-        eventListernerController()
+        const reactionForm = document.querySelectorAll(`[id*="reactionForm"]`)
+        for (let i = 0; i < reactionForm.length; i++) {
+            reactionForm[i].addEventListener('click', (event) => {
+                // event.preventDefault()
+                valueArray = event.target['value'].split(" ")
+
+                const reactionData = {
+                    id: parseInt(valueArray[1]),
+                    reactions: valueArray[0]
+                }
+                const options = {
+                    method: 'POST',
+                    body: JSON.stringify(reactionData),
+                    headers: {
+                        "Content-Type": "application/json",
+                    }
+                }
+                fetch('http://localhost:3000/updatearticlereaction', options).then(()=> buildDeck())
+                console.log('test')
+                
+            })
+        }
     });
     observer.observe(cardDeck, { childList: true })
-    
+
 
 
 }
@@ -245,8 +266,8 @@ const articleForm = document.querySelector('#userForm');
 articleForm.addEventListener('submit', (event) => {
     event.preventDefault()
     submitArticle(event);
-    setTimeout(buildDeck(),500)
-    
+    setTimeout(buildDeck(), 500)
+
 })
 
 
