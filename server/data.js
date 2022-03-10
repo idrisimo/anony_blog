9fd3f9ec9a35526aae87d108fd6d518098a42539
Ikenna Agulobi <ikennaww@yahoo.fr>
2022-03-10 15:56:34 +0000

    typo removed from index html

--- server/data.js
+++ server/data.js
@@ -1,12 +1,9 @@
-// const articles =  [
-//     { id:1, title: "article 1", createdAt:"", description:"Very good article 1",comments: ["Good post", "I like this post"],reactions: ["U+1F600", "U+1F600"], giphy: [null]},
-//     { id:2, title: "article 2", createdAt:"", description:"Very good article 2",comments: ["I hate this post", "bad post"],reactions:  [null], giphy: [null]},
-//     { id:3, title: "article 3", createdAt:"", description:"Very good article 3",comments: ["OK post", "Not a bad post"],reactions:  ["U+1F600"], giphy: [null]},
-//     { id:4, title: "article 4", createdAt:"", description:"Very good article 4",comments: ["Great post! ", "I relate to this post"],reactions: ["U+1F600", "U+1F600", "U+1F602"], giphy: [null]}
-// ]
-
 const articles =  [
-        { id:1, title: "article 1", createdAt:"", description:"Very good article 1",comments: ["Good post", "I like this post"],reactions: ["U+1F600", "U+1F600"], giphys: [null]}
-    ]
+    { id:1, title: "article 1", createdAt:"", description:"Very good article 1",comments: ["Good post", "I like this post"],reactions: ["U+1F600", "U+1F600"], giphys: [null]},
+    { id:2, title: "article 2", createdAt:"", description:"Very good article 2",comments: ["I hate this post", "bad post"],reactions:  [null], giphys: [null]},
+    { id:3, title: "article 3", createdAt:"", description:"Very good article 3",comments: ["OK post", "Not a bad post"],reactions:  ["U+1F600"], giphys: [null]},
+    { id:4, title: "article 4", createdAt:"", description:"Very good article 4",comments: ["Great post! ", "I relate to this post"],reactions: ["U+1F600", "U+1F600", "U+1F602"], giphys: [null]}
+]
+
 
 module.exports = articles
