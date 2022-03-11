d06f379f32e457eba1ad80b4fc7d7e174c5ff2bd
Marco <marcoporcaro87@gmail.com>
2022-03-11 12:42:22 +0000

    fixing bundle.js

--- client/bundle.js
+++ client/bundle.js
@@ -1,197 +1,10 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-// shim for using process in browser
-var process = module.exports = {};
-
-// cached from whatever global is present so that test runners that stub it
-// don't break things.  But we need to wrap it in a try catch in case it is
-// wrapped in strict mode code which doesn't define any globals.  It's inside a
-// function because try/catches deoptimize in certain engines.
-
-var cachedSetTimeout;
-var cachedClearTimeout;
-
-function defaultSetTimout() {
-    throw new Error('setTimeout has not been defined');
-}
-function defaultClearTimeout () {
-    throw new Error('clearTimeout has not been defined');
-}
-(function () {
-    try {
-        if (typeof setTimeout === 'function') {
-            cachedSetTimeout = setTimeout;
-        } else {
-            cachedSetTimeout = defaultSetTimout;
-        }
-    } catch (e) {
-        cachedSetTimeout = defaultSetTimout;
-    }
-    try {
-        if (typeof clearTimeout === 'function') {
-            cachedClearTimeout = clearTimeout;
-        } else {
-            cachedClearTimeout = defaultClearTimeout;
-        }
-    } catch (e) {
-        cachedClearTimeout = defaultClearTimeout;
-    }
-} ())
-function runTimeout(fun) {
-    if (cachedSetTimeout === setTimeout) {
-        //normal enviroments in sane situations
-        return setTimeout(fun, 0);
-    }
-    // if setTimeout wasn't available but was latter defined
-    if ((cachedSetTimeout === defaultSetTimout || !cachedSetTimeout) && setTimeout) {
-        cachedSetTimeout = setTimeout;
-        return setTimeout(fun, 0);
-    }
-    try {
-        // when when somebody has screwed with setTimeout but no I.E. maddness
-        return cachedSetTimeout(fun, 0);
-    } catch(e){
-        try {
-            // When we are in I.E. but the script has been evaled so I.E. doesn't trust the global object when called normally
-            return cachedSetTimeout.call(null, fun, 0);
-        } catch(e){
-            // same as above but when it's a version of I.E. that must have the global object for 'this', hopfully our context correct otherwise it will throw a global error
-            return cachedSetTimeout.call(this, fun, 0);
-        }
-    }
-
-
-}
-function runClearTimeout(marker) {
-    if (cachedClearTimeout === clearTimeout) {
-        //normal enviroments in sane situations
-        return clearTimeout(marker);
-    }
-    // if clearTimeout wasn't available but was latter defined
-    if ((cachedClearTimeout === defaultClearTimeout || !cachedClearTimeout) && clearTimeout) {
-        cachedClearTimeout = clearTimeout;
-        return clearTimeout(marker);
-    }
-    try {
-        // when when somebody has screwed with setTimeout but no I.E. maddness
-        return cachedClearTimeout(marker);
-    } catch (e){
-        try {
-            // When we are in I.E. but the script has been evaled so I.E. doesn't  trust the global object when called normally
-            return cachedClearTimeout.call(null, marker);
-        } catch (e){
-            // same as above but when it's a version of I.E. that must have the global object for 'this', hopfully our context correct otherwise it will throw a global error.
-            // Some versions of I.E. have different rules for clearTimeout vs setTimeout
-            return cachedClearTimeout.call(this, marker);
-        }
-    }
-
-
-
-}
-var queue = [];
-var draining = false;
-var currentQueue;
-var queueIndex = -1;
-
-function cleanUpNextTick() {
-    if (!draining || !currentQueue) {
-        return;
-    }
-    draining = false;
-    if (currentQueue.length) {
-        queue = currentQueue.concat(queue);
-    } else {
-        queueIndex = -1;
-    }
-    if (queue.length) {
-        drainQueue();
-    }
-}
-
-function drainQueue() {
-    if (draining) {
-        return;
-    }
-    var timeout = runTimeout(cleanUpNextTick);
-    draining = true;
-
-    var len = queue.length;
-    while(len) {
-        currentQueue = queue;
-        queue = [];
-        while (++queueIndex < len) {
-            if (currentQueue) {
-                currentQueue[queueIndex].run();
-            }
-        }
-        queueIndex = -1;
-        len = queue.length;
-    }
-    currentQueue = null;
-    draining = false;
-    runClearTimeout(timeout);
-}
-
-process.nextTick = function (fun) {
-    var args = new Array(arguments.length - 1);
-    if (arguments.length > 1) {
-        for (var i = 1; i < arguments.length; i++) {
-            args[i - 1] = arguments[i];
-        }
-    }
-    queue.push(new Item(fun, args));
-    if (queue.length === 1 && !draining) {
-        runTimeout(drainQueue);
-    }
-};
-
-// v8 likes predictible objects
-function Item(fun, array) {
-    this.fun = fun;
-    this.array = array;
-}
-Item.prototype.run = function () {
-    this.fun.apply(null, this.array);
-};
-process.title = 'browser';
-process.browser = true;
-process.env = {};
-process.argv = [];
-process.version = ''; // empty string to avoid regexp issues
-process.versions = {};
-
-function noop() {}
-
-process.on = noop;
-process.addListener = noop;
-process.once = noop;
-process.off = noop;
-process.removeListener = noop;
-process.removeAllListeners = noop;
-process.emit = noop;
-process.prependListener = noop;
-process.prependOnceListener = noop;
-
-process.listeners = function (name) { return [] }
-
-process.binding = function (name) {
-    throw new Error('process.binding is not supported');
-};
-
-process.cwd = function () { return '/' };
-process.chdir = function (dir) {
-    throw new Error('process.chdir is not supported');
-};
-process.umask = function() { return 0; };
-
-},{}],2:[function(require,module,exports){
 const { API_URL } = require("./url")
 
 
 function getAllArticles() {
     return fetch(`${API_URL}/articles`).then((response) => response.json()).catch(console.warn)
 }
-
 
 function buildDeck() {
     console.log('building deck')
@@ -344,7 +157,7 @@ function cardTemplate(data, index) {
         </div>
         <div class="card-footer">
         
-            <div>
+            <div class="comment-cont">
                 <a class="comment-icon-format me-3" data-bs-toggle="collapse" href="#commentCollapse${index}" role="button" aria-expanded="false" aria-controls="commentCollapse${index}">
 
                     <i class="fa-solid fa-comment">
@@ -392,7 +205,7 @@ function cardTemplate(data, index) {
 module.exports = { buildDeck, submitReaction, showComments, getAllArticles, removeStaleDeck, sendComments, constructReactionData }
 
 
-},{"./url":5}],3:[function(require,module,exports){
+},{"./url":4}],2:[function(require,module,exports){
 const { buildDeck } = require("./cardCreation");
 const { API_URL } = require("./url");
 
@@ -433,35 +246,13 @@ function closeModalOnSuccess() {
     modal.hide()
 }
 
-// function successAlert(message, type) {
-//     const alertWrapper = document.createElement('div')
-//     alertWrapper.setAttribute('class', `alert alert-${type} alert-dismissible`)
-//     alertWrapper.setAttribute('role', 'alert')
-
-//     const btn = document.createElement('button')
-//     btn.setAttribute('type', 'button')
-//     btn.setAttribute('class', 'btn-close')
-//     btn.setAttribute('data-bs-dismiss', 'alert')
-//     btn.setAttribute('aria-label', 'Close')
-
-//     alertWrapper.textContent = message
-
-//     alertWrapper.append(btn)
-//     const submitAlert = document.getElementById('submitAlert')
-//     submitAlert.append(alertWrapper)
-// }
-
 module.exports = { submitArticle }
 
-},{"./cardCreation":2,"./url":5}],4:[function(require,module,exports){
-(function (process){(function (){
+},{"./cardCreation":1,"./url":4}],3:[function(require,module,exports){
 const { submitArticle } = require("./handler");
 const { buildDeck, submitReaction, showComments } = require("./cardCreation");
 const API_URL = require('./url')
 
-console.log(process.env.PORT)
-
-console.log(process.env)
 window.onload = () => {
 
     buildDeck()
@@ -476,11 +267,7 @@ window.onload = () => {
         showComments()
     });
     observer.observe(cardDeck, { childList: true })
-
-
-
 }
-
 
 // selectors
 const articleForm = document.querySelector('#userForm');
@@ -491,42 +278,16 @@ articleForm.addEventListener('submit', (event) => {
     submitArticle(event);
 })
 
-// class SubmitClass {
-//     submitFunction() {
-//         // selectors
-//         const articleForm = document.querySelector('#userForm');
-
-//         // event listeners
-
-//         articleForm.addEventListener('submit', (event) => {
-//             event.preventDefault()
-//             submitArticle(event);
-//             this.submitEvent.bind(this)
-//         })
-//     }
-//     submitEvent() {
-
-//     }
-// }
-
-// const submitArticleForm = new SubmitClass();
-// submitArticleForm
-
-// module.exports = {SubmitClass}
-
-module
-
-}).call(this)}).call(this,require('_process'))
-},{"./cardCreation":2,"./handler":3,"./url":5,"_process":1}],5:[function(require,module,exports){
+},{"./cardCreation":1,"./handler":2,"./url":4}],4:[function(require,module,exports){
 
 // development
-const API_URL = 'http://localhost:3000'
+// const API_URL = 'http://localhost:3000'
 
 // production
-// const API_URL = 'https://api-anonyblogfp.herokuapp.com'
+const API_URL = 'https://api-anonyblogfp.herokuapp.com'
 
 
 
 module.exports = {API_URL};
 
-},{}]},{},[4]);
+},{}]},{},[3]);
