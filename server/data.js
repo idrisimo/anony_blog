fa132c1e57e749209f5444e214b29d9c7f0a03db
Ikenna Agulobi <ikennaww@yahoo.fr>
2022-03-11 02:01:25 +0000

    added read article link button and edited the data files in data server

--- server/data.js
+++ server/data.js
@@ -1,19 +1,18 @@
 const articles =  [
-
-    { id:1, title: "Why every one should learn how to code",
+   { id:1, title: "Why every one should learn how to code",
     createdAt:"", description:"Technology is everywhere in the modern world and continues to develop at an astonishing rate. Coding already plays a key part in this technological evolution and will become increasingly important in light of new robotics and automated technologies. Learning to code will give you the tools you need to go on and be part of shaping the future. This could include roles in RPA, building automated systems for global banks or healthcare organisations, or even helping to improve the tech in self-driving cars - this is just scratching the surface!",
     comments: ["Good post", "I like this post"],reactions: ["U+1F600", "U+1F600"], giphys: [null]},
     { id:2, title: "The art of self acceptance",
-    createdAt:"Monday, Mar 07, 2022",
+    createdAt:"",
     description:"It is important to remind ourselves that no one is perfect.We all have strengths and weaknesses and we certainly possess the resources to work on improving ourselves.Each of us is unique and has specific talents and abilities to offer. We are all here, in the words of Walt Whitman, ‘to contribute a verse’.In order to appreciate yourself it is up to you to discover what makes you unique and to further develop those talents. We have a responsibility to ourselves to do so.You cannot sit around and wait for approval from others. Work on accepting yourself. You are the only ‘you’ t",
     comments: ["I hate this post", "bad post"],reactions:  [null], giphys: [null]},
     { id:3, title: "How to start a business",
-    createdAt:"Thursday, Mar 10, 2022",
+    createdAt:"",
     description:"Your first step in learning how to start a business is to identify a problem and solution. This is because successful startups begin from business ideas that fill the needs of a group of customers. But your idea doesn’t always have to be a new one. You can update existing products or services in a way that’s better for the consumer.For instance, Apple started from Steve Jobs’ original idea for a computer and has since created enhanced versions that better fit the market. They’ve also continued to evolve newer products like iPhones and iPads, making them more useful with each update.One example is how they’re adding a keyboard for iPads that’ll make them easier to use like a laptop.2 All these innovations by Apple led to them being worth of over a billion dollars. ",comments: [ "Not a bad post"],reactions:  ["U+1F600"], giphys: [null]},
-    { id:4, title: "why happiness matter", createdAt:"Friday, Mar 11, 2022",
+    { id:4, title: "why happiness matter", createdAt:"",
     description:"Happiness is more than a good feeling or a yellow smiley face. It's the feeling of truly enjoying your life, and the desire to make the very best of it. Happiness is the 'secret sauce' that can help us be and do our best.Besides feeling good, positive emotions do good things for our brains and bodies. They lower stress hormones, help ease anxiety and depression, and improve our immune system.Feeling some positive emotions every day has a big effect on our happiness and well-being. That's why it's so important to do things that give us positive feelings. Even simple actions like playing with a child or a pet or going for a walk outdoors can inspire these feelings.Knowing how to manage our negative emotions is also key to happiness. Difficult emotions are a fact of life. But the way we handle them makes all the difference.",
     comments: ["Great post! ", "I relate to this post"],reactions: ["U+1F600", "U+1F600", "U+1F602"], giphys: [null]}
-]
+ ]
 
 
 
